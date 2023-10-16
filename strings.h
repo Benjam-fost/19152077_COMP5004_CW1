389bbf3482a215d5a354d7ace2f3a873d232f9f9
Benjam-fost <129466520+Benjam-fost@users.noreply.github.com>
2023-10-16 22:45:49 +0100

    Initial upload
    90% done, last part of ex1 to do.

---
+++ strings.h
@@ -0,0 +1,26 @@
+//
+// Created by DrMark on 10/4/2017.
+//
+
+#ifndef TEXTADV_STRINGS_H
+#define TEXTADV_STRINGS_H
+
+
+#include <string>
+
+const std::string r1name = "Room 1";
+const std::string r1desc = "You are in room 1. It's really quite boring, but then, it's just for testing really. There's a passage to the north.";
+const std::string r2name = "Blue Room";
+const std::string r2desc = "You are in the blue room. It's blue. That's about all though. There's a passage to the south.";
+const std::string r3name = "Red Room";
+const std::string r3desc = "You are in the red room. It's red. That's about all though. There's a passage to the north.\"";
+const std::string r4name = "Green Room";
+const std::string r4desc = "You are in the green room. It's green. That's about all though. There's a passage to the west.\"";
+const std::string r5name = "Purple Room";
+const std::string r5desc = "You are in the purple room. It's purple. That's about all though, except from some fluffy pillows. There's a passage to the east.\"";
+
+const std::string badExit = "You can't go that way.";
+const std::string badCommand = "I don't understand that.";
+
+
+#endif //TEXTADV_STRINGS_H
